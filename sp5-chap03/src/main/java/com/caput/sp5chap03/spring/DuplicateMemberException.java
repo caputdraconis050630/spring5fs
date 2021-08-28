b5cd73f1080b8d39b16f2a9cd15660c5e810b255
caputdraconis050630 <caputdraconis@kakao.com>
2021-08-28 18:18:04 +0900

    .

---
+++ sp5-chap03/src/main/java/com/caput/sp5chap03/spring/DuplicateMemberException.java
@@ -0,0 +1,8 @@
+package com.caput.sp5chap03.spring;
+
+public class DuplicateMemberException extends RuntimeException {
+
+    public DuplicateMemberException(String message) {
+        super(message);
+    }
+}
---
+++ sp5-chap03/src/main/java/com/caput/sp5chap03/spring/MemberNotFoundException.java
@@ -0,0 +1,5 @@
+package com.caput.sp5chap03.spring;
+
+public class MemberNotFoundException extends RuntimeException{
+
+}

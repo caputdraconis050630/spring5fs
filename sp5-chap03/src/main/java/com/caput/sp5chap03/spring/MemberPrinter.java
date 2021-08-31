00b1102c40a522ab2c44e8c92eab1503e5d33e20
caputdraconis050630 <caputdraconis@kakao.com>
2021-09-01 00:57:28 +0900

    Fix MemberPrinter Class

--- sp5-chap03/src/main/java/com/caput/sp5chap03/spring/MemberPrinter.java
+++ sp5-chap03/src/main/java/com/caput/sp5chap03/spring/MemberPrinter.java
@@ -3,6 +3,6 @@ package com.caput.sp5chap03.spring;
 public class MemberPrinter {
 
     public void print(Member member) {
-        System.out.printf("회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%tF\n", member.getId(), member.getEmail(), member.getEmail(), member.getRegisterDateTime());
+        System.out.printf("회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%tF\n", member.getId(), member.getEmail(), member.getName(), member.getRegisterDateTime());
     }
 }

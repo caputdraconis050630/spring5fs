30611c3b3c282c7fc153d72fa71839a272041af2
Guntak Kim <caputdraconis@kakao.com>
2022-04-11 14:39:23 +0900

    hoit

--- sp5-chap03/src/main/java/com/caput/sp5chap03/spring/DuplicateMemberException.java
+++
@@ -1,8 +0,0 @@
-package main.java.spring;
-
-public class DuplicateMemberException extends RuntimeException {
-
-    public DuplicateMemberException(String message) {
-        super(message);
-    }
-}
